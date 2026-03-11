e0777ea8224b318f2498d53ea48695daa76adb1a
dxz456852-netizen <dxz456852@gmail.com>
2026-03-11 14:50:50 +0900

    DebateCreate-InvitePage재연동

--- server/src/routes/debate.routes.js
+++ server/src/routes/debate.routes.js
@@ -1,36 +1,12 @@
-// import { Router } from 'express';
-// import { createDebate, getDebate, listDebates, joinByInvite } from '../controllers/debate.controller.js';
-// import { requireAuth } from '../middleware/auth.middleware.js';
-
-// const router = Router();
-
-// router.get('/', listDebates);
-// router.get('/:id', getDebate);
-// router.post('/', requireAuth, createDebate);
-// router.post('/join/:inviteCode', requireAuth, joinByInvite);
-
-// export default router;
-
 import { Router } from 'express';
-import { 
-  createDebate, 
-  getDebate, 
-  listDebates, 
-  joinByInvite,
-  getDebateByInviteCode
-} from '../controllers/debate.controller.js';
-
+import { createDebate, getDebate, listDebates, getDebateByInviteCode, joinByInvite } from '../controllers/debate.controller.js';
 import { requireAuth } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
 router.get('/', listDebates);
-
-// ⭐ 반드시 이걸 먼저
-router.get('/invite/:inviteCode', getDebateByInviteCode);
-
+router.get('/invite/:inviteCode', requireAuth, getDebateByInviteCode);
 router.get('/:id', getDebate);
-
 router.post('/', requireAuth, createDebate);
 router.post('/join/:inviteCode', requireAuth, joinByInvite);
 
